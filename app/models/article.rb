fbc2175f61b2ab23ff177766954c22bf6d2e4cec
Ana Hernandez <ana.hernandez292@gmail.com>
2014-07-11 16:31:41 -0700

    removed duplicate code.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,12 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true,
-                    length: { minimum: 5 }
-                              
                     
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
                                 
-has_many :comments
-validates :title,  presence: true,
-          length: { minimum: 5 }
+
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+                  length: { minimum: 5 }
+                  
+
 end
