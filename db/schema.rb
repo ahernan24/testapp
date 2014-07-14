6142e042f1fdc39e5bf69e4ae278493e2bd0eb1e
Ana Hernandez <ana.hernandez292@gmail.com>
2014-07-14 14:03:54 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711222636) do
+ActiveRecord::Schema.define(version: 20140714201732) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -31,9 +31,11 @@ ActiveRecord::Schema.define(version: 20140711222636) do
     t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
